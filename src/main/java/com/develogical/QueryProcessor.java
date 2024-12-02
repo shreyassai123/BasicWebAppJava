f24097bb015a8252aa2bc7ea585ee630b3a39aa6
Shreyas Sai <shreyassai2@gmail.com>
2024-12-02 17:17:43 +0000

    added queries

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -2,6 +2,18 @@ package com.develogical;
 
 public class QueryProcessor {
 
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public String process(String query) {
 
         if (query.toLowerCase().contains("shakespeare")) {
@@ -12,6 +24,20 @@ public class QueryProcessor {
 
         if (query.contains("your name")) {
             return "RobTest";
+        }
+
+        if (query.contains("What is") && query.contains("plus")) {
+            String[] words = query.split(" ");
+            int num1 = Integer.parseInt(words[2]); 
+            int num2 = Integer.parseInt(words[4]);  
+            int num3 = Integer.parseInt(words[6]);  
+
+            int sum = num1 + num2 + num3;
+            return String.valueOf(sum);
+        }
+
+        if(query.contains("Which of the following numbers are primes:")) {
+            
         }
 
         return "";
